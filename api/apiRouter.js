3adab9cdbba2e85ffe3d9de2d691b5f86dc12056
Ashley Dunham <xenocide21@gmail.com>
2020-03-06 18:57:59 -0800

    finished routing

--- api/apiRouter.js
+++ api/apiRouter.js
@@ -4,14 +4,38 @@ const db = knex(require('../knexfile'))
 const express = require('express')
 const router = express.Router()
 
-const projectRouter = require('')
-const resourceRouter = require('')
+const prRouter = require('./project-resources/prRouter');
+const contextsRouter = require('./contexts/contextsRouter');
+const projectsRouter = require('./projects/projectsRouter')
+const resourcesRouter = require('./resources/resourcesRouter')
+const tasksRouter = require('./tasks/tasksRouter');
+const todoRouter = require('./todos/todoRouter');
 
-router.use('/projects', projectRouter)
-router.use('/resources', resourceRouter)
+router.use('/project-resources', prRouter);
+router.use('/contexts', contextsRouter);
+router.use('/projects', projectsRouter);
+router.use('/resources', resourcesRouter);
+router.use('/tasks', tasksRouter);
+router.use('/todos', todoRouter);
+
+const routesInfo = `
+  <h1>API</h1>
+  <div>
+    <h2>Projects</h2>
+  </div>
+  <div>
+    <h2>Resources</h2>
+  </div>
+  <div>
+   <h2>Tasks</h2>
+  </div>
+  <div>
+   <h2>Contexts</h2>
+  </div>
+`
 
 router.get('/', (req, res) => {
-    res.status(200).send('Connected to api router')
+    res.status(200).send(routesInfo)
 })
 
 router.use((req, res) => {

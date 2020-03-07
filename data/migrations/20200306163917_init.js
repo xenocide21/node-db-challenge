a17b9191fe2c6ebdbccfa0882e747d8097159fe3
Ashley Dunham <xenocide21@gmail.com>
2020-03-06 17:00:11 -0800

    initial migration made to create tables

--- data/migrations/20200306163917_init.js
+++ data/migrations/20200306163917_init.js
@@ -1,8 +1,33 @@
-
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+    await knex.schema
+        .createTable('projects', tbl => {
+            tbl.increments().unsigned()
+            tbl.string('name', 128).notNullable()
+            tbl.text('description')
+            tbl.boolean('completed').notNullable().defaultTo(0)
+        })
+        .createTable('tasks', tbl => {
+            tbl.increments().unsigned()
+            tbl.text('description').notNullable()
+            tbl.text('notes')
+            tbl.boolean('completed').notNullable().defaultTo(0)
+            tbl.integer('projectId')
+                .unsigned()
+                .references('id')
+                .inTable('projects')
+                .onDelete('CASCADE')
+                .onUpdate('CASCADE')
+        })
+        .createTable('resources', tbl => {
+            tbl.increments().unsigned()
+            tbl.string('name', 128).notNullable().defaultTo(0)
+            tbl.text('description')
+        })
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+    await knex.schema
+        .dropTableIfExists('resources')
+        .dropTableIfExists('tasks')
+        .dropTableIfExists('projects')
 };
